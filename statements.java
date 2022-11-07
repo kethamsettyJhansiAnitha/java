8c191863f77ac0bf571dae508709996fae610329
kethamsettyJhansiAnitha <116719962+kethamsettyJhansiAnitha@users.noreply.github.com>
2022-11-07 17:21:03 +0530

    Add files via upload

---
+++ statements.java
@@ -0,0 +1,13 @@
+package conditional;
+
+public class statements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = -10;
+		if (i<0)
+		System.out.println( "The given number is negative");
+	}
+}
+
+// if block Executes only true condition//
