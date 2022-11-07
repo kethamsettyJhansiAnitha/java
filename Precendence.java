00c5cb91890a73eb09bef2bbd8152d7eb54f30be
kethamsettyJhansiAnitha <116719962+kethamsettyJhansiAnitha@users.noreply.github.com>
2022-11-07 15:27:12 +0530

    Add files via upload

---
+++ Precendence.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Precendence {
+	
+		public static void main(String[] args) {
+			System.out.println(3+3*2);
+			System.out.println(3+3-2);
+			System.out.println(3*3*2);
+			System.out.println("-------");
+	}
+			
+		}
+
+
