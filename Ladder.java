566e36456f17e092d6800275e324a5d4e06f84d3
kethamsettyJhansiAnitha <116719962+kethamsettyJhansiAnitha@users.noreply.github.com>
2022-11-07 17:21:41 +0530

    Add files via upload

---
+++ Ladder.java
@@ -0,0 +1,19 @@
+package conditional;
+
+public class Ladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		
+		String Name="Anitha";
+		if (Name== "Sweatha") {
+			System.out.println(" Name is Sweatha");}
+		else if( Name=="Chinnu") {
+			System.out.println(" Name is chinnu");}
+		else
+		{System.out.println("Name is Anitha");
+		}
+		}
+			
+		}
+
