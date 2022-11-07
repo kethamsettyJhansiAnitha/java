fd6ba2d3d63f6531cd2a733ac4416affa355ca90
kethamsettyJhansiAnitha <116719962+kethamsettyJhansiAnitha@users.noreply.github.com>
2022-11-07 17:18:17 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,20 @@
+package conditional;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		if(a>b)
+			System.out.println("a is greater than b");
+		else
+		{
+			System.out.println("a is less than b");
+		}
+	}
+
+}
+//if else
+//Statement: Block of code to be executed, it performs two operations for a single condition, 
+//if condition is true if block is executed otherwise else block is executed
