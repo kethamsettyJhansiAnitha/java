614dc6d58f528a5a940a0cc00454cffe653df095
kethamsettyJhansiAnitha <116719962+kethamsettyJhansiAnitha@users.noreply.github.com>
2022-11-09 12:24:27 +0530

    Add files via upload

---
+++ String_test.java
@@ -0,0 +1,18 @@
+package string;
+
+public class String_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String S =new String("Manvith");
+S.concat("Software");
+System.out.println(S);
+	}
+//	String is not but sequence of character it represents a double codes.
+//	String Immutable means changes are not applicable.
+//	equals() method  present in only object class also meant for reference Comparision
+//	are address comparsion.
+//	
+	 
+
+}
