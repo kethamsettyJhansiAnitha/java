0bbf6b3e2304f3a855c2fa7a57d31be351849b71
kethamsettyJhansiAnitha <116719962+kethamsettyJhansiAnitha@users.noreply.github.com>
2022-11-08 23:04:22 -0800

    Update String_test.java

--- String_test.java
+++ String_test.java
@@ -9,10 +9,15 @@ S.concat("Software");
 System.out.println(S);
 	}
 //	String is not but sequence of character it represents a double codes.
-//	String Immutable means changes are not applicable.
+//	String is immutable means changes are not applicable in strings.
 //	equals() method  present in only object class also meant for reference Comparision
 //	are address comparsion.
-//	
-	 
+//	equals method
+		String s1=  ("chinnu");
+		String s2=  ("chinnu");
+		String s3=  ("Ani");
+		System.out.println(s1==s2);//true
+		System.out.println(s2==s3);//false
+		System.out.println(s3==s1);//false
 
 }
