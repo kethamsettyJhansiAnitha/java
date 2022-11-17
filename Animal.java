937647297ec069734a33379af6846be35a2fc9bc
Kethansetty  jhansi Anitha <116719962+kethamsettyJhansiAnitha@users.noreply.github.com>
2022-11-17 00:36:41 -0800

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -6,7 +6,17 @@ public class Animal {
 		this ("Cat","milk");}
 		public static void main(String args[]) {
 		animal a=new animal("Cat","milk");
-		System.out.println(a);
+	animal b=new animal("Ant","Sugar");
+	animal c=new animal("Dog","Pedigree");
+	animal d=new animal("Cow","Grass");
+	animal e=new animal("Rabbit","Carrot");
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(e);
+	
+		
 		}
 	}
 
