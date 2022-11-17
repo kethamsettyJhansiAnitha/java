a6ba4093707105df2023b499703dde607505b2b0
Kethansetty  jhansi Anitha <116719962+kethamsettyJhansiAnitha@users.noreply.github.com>
2022-11-17 00:40:25 -0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,15 +1,19 @@
 package record;
-
 public class Student {
-	record student(String name,String course,int rollno) {
+		record student(String name,String course,int rollno) {
 		student(){
-			this("Jhansi" , "Computer Science",504);}
+		this("Jhansi" , "Computer Science",504);}
 		public static void main(String args[]) {
-			student s=new student("Jhansi" , "Computer Science" ,504);
-			System.out.println(s);
+		student s=new student("pooji" , "Computer Science" ,501);
+		student s1=new student("Pavan" , "Computer Science" ,502);
+		student s2=new student("Anitha" , "Computer Science" ,503);
+		student s3=new student("Jhansi" , "Computer Science" ,504);
+		student s4=new student("Maneesha", "Computer Science" ,505);
+		System.out.println(s);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		System.out.println(s4);
 			
 		}
 		}
-
-
-}
