9d6ded5dfac81c81f9874799f6011c397ae588eb
Kethansetty  jhansi Anitha <116719962+kethamsettyJhansiAnitha@users.noreply.github.com>
2022-12-09 23:52:33 -0800

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,9 +1,25 @@
 
 public class Dog {
-
+public String breed ="German sharapad";
+public double height= 10.5;
+public int weight =5;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Dog dog = new Dog();
+		System.out.println(dog.breed);
+		System.out.println(dog.height);
+		System.out.println(dog.weight);
+	}
 
+
+//Object:Object is a piece of code Which represent the real life entity.
+//An object is an entity,Each object contains data and code to manipulate the data.
+//Objects it gives life to classes.
+//Class:Class is nothing but a blueprint which contains the variables and methods information.
+//Class:Class is nothing but a blueprint.
+//Class is nothing but a collection of objects.
+//Without class object does not exist.
+//Without object we can use only static functions of classes.
+//Objects and classes are the main building blocks of the implementation of the OOAD programs
 	}
 
 }
